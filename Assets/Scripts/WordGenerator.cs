46e30fc62c7813ad6eb87eb7adf6491f4a974a61
XDenizX <denizik1@mail.ru>
2023-01-16 18:54:27 +0300

    Added word generator.

---
+++ Assets/Scripts/WordGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+public class WordGenerator
+{
+    private readonly WordsStorage _wordsStorage;
+    private readonly Random _random;
+    
+    public WordGenerator(WordsStorage wordsStorage)
+    {
+        _wordsStorage = wordsStorage;
+    }
+    
+    public string GenerateBeginning(int charCount)
+    {
+        var word = TakeRandomWord();
+        return word[..charCount];
+    }
+    
+    public string GenerateEnding(int charCount)
+    {
+        var word = TakeRandomWord();
+        return word[^charCount..];
+    }
+
+    private string TakeRandomWord()
+    {
+        var randomIndex = _random.Next(_wordsStorage.Words.Count);
+        return _wordsStorage.Words.ElementAt(randomIndex);
+    }
+}
