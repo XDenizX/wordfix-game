c5ce7ee455678c98b65bcad98466b4e2575833ea
XDenizX <denizik1@mail.ru>
2023-01-16 18:54:12 +0300

    Added words storage.

---
+++ Assets/Scripts/WordsStorage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class WordsStorage
+{
+    public HashSet<string> Words { get; }
+
+    private WordsStorage(string language)
+    {
+        var filepath = Path.Combine(Paths.DictionariesRelativePath, language);
+        Words = LoadWords(filepath);
+    }
+
+    private HashSet<string> LoadWords(string filepath)
+    {
+        return File.ReadAllLines(filepath).ToHashSet();
+    }
+
+    public bool Exist(string word)
+    {
+        return Words.Contains(word);
+    }
+}
