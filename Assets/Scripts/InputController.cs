f7601f6b408d496800d22ae4848913ec3bc797ba
XDenizX <denizik1@mail.ru>
2023-01-16 18:55:18 +0300

    Implemented input controller for the game.

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public delegate void KeyboardEventHandler(object sender, char pressedCharacter);
+
+    public event KeyboardEventHandler KeyPressed;
+    public event EventHandler ReturnPressed;
+    public event EventHandler BackspacePressed;
+
+    private bool _isKeyPressed;
+
+    public void OnGUI()
+    {
+        Event currentEvent = Event.current;
+
+        if (currentEvent.type == EventType.KeyUp)
+            _isKeyPressed = false;
+        
+        if (currentEvent.type != EventType.KeyDown)
+            return;
+
+        if (_isKeyPressed)
+            return;
+
+        switch (currentEvent.keyCode)
+        {
+            case KeyCode.Return:
+            {
+                ReturnPressed?.Invoke(this, EventArgs.Empty);
+                _isKeyPressed = true;
+                break;
+            }
+            case KeyCode.Backspace:
+            {
+                BackspacePressed?.Invoke(this, EventArgs.Empty);
+                _isKeyPressed = true;
+                break;
+            }
+            default:
+            {
+                if (currentEvent.character != 0)
+                {
+                    KeyPressed?.Invoke(this, currentEvent.character);
+                    _isKeyPressed = true;
+                }
+                break;
+            }
+        }
+    }
+}
