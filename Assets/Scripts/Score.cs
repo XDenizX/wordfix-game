5a984d28ed1f358e456e2abebcc1d4bd898a2bb3
XDenizX <denizik1@mail.ru>
2023-02-08 01:54:01 +0300

    Added text block and score components.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private TextMeshProUGUI _scoreText;
+
+    private bool _hasText;
+    
+    private int _points;
+    public int Points
+    {
+        get => _points;
+        set
+        {
+            _points = value;
+            if (_hasText)
+                _scoreText.text = value.ToString();
+        }
+    }
+
+    private void Start()
+    {
+        _scoreText = GetComponent<TextMeshProUGUI>();
+        _hasText = _scoreText != null;
+    }
+}
---
+++ Assets/Scripts/TextBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBlock : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterBlock characterBlockPrefab;
+
+    private readonly List<CharacterBlock> _characterBlocks = new();
+    
+    private string _text;
+    public string Text
+    {
+        get => _text;
+        set
+        {
+            _text = value;
+            _characterBlocks.ForEach(block => Destroy(block.gameObject));
+            _characterBlocks.Clear();
+            
+            foreach (char character in value)
+            {
+                var block = Instantiate(characterBlockPrefab, transform);
+                block.Character = character;
+                _characterBlocks.Add(block);
+            }
+        }
+    }
+}
