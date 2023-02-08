51352ed37a018d93d04c308b282a1b5da0274e65
XDenizX <denizik1@mail.ru>
2023-02-08 14:30:12 +0300

    Added a game settings DTO.

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,6 @@
+﻿public class GameSettings
+{
+    public string DictionaryLanguage { get; set; } = "ru-ru";
+    public bool IsMusicEnabled { get; set; } = true;
+    public float MusicVolume { get; set; } = 0.5f;
+}
