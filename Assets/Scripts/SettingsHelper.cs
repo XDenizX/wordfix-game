bc9db60defd7d367291121399a011ce38ea1476b
XDenizX <denizik1@mail.ru>
2023-02-08 14:17:42 +0300

    Implemented a settings helper.

---
+++ Assets/Scripts/SettingsHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+public static class SettingsHelper
+{
+    public static GameSettings Load(string filepath)
+    {
+        var text = File.ReadAllText(filepath);
+        return JsonConvert.DeserializeObject<GameSettings>(text);
+    }
+    
+    public static bool TryLoad(string filepath, out GameSettings settings)
+    {
+        try
+        {
+            var text = File.ReadAllText(filepath);
+            settings = JsonConvert.DeserializeObject<GameSettings>(text);
+            return true;
+        }
+        catch
+        {
+            settings = null;
+            return false;
+        }
+    }
+
+    public static void Save(GameSettings setting, string filepath)
+    {
+        var jsonData = JsonConvert.SerializeObject(setting, Formatting.Indented);
+        File.WriteAllText(filepath, jsonData);
+    }
+}
