2b462688f3759465a01ccb8327cdfd7351953155
XDenizX <denizik1@mail.ru>
2023-02-08 14:18:08 +0300

    Added a settings manager.

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    public GameSettings Settings { get; private set; }
+
+    private void Start()
+    {
+        SettingsHelper.TryLoad(Paths.SettingsRelativePath, out var settings);
+        Settings = settings ?? new GameSettings();
+    }
+
+    private void OnApplicationQuit()
+    {
+        SettingsHelper.Save(Settings, Paths.SettingsRelativePath);
+    }
+}
