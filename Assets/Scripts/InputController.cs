273d5f12309356984aca682f1878c79ab7aeb223
XDenizX <denizik1@mail.ru>
2023-02-09 16:36:07 +0300

    Improved an input controller.

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,52 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
-    public delegate void KeyboardEventHandler(object sender, char pressedCharacter);
-
-    public event KeyboardEventHandler KeyPressed;
+    private const char NullCharacter = default;
+    private const char ReturnCharacter = (char)10;
+    
+    public event EventHandler<char> KeyPressed;
     public event EventHandler ReturnPressed;
     public event EventHandler BackspacePressed;
 
-    private bool _isKeyPressed;
+    private readonly List<KeyCode> _pressedKeyCodes = new();
 
     public void OnGUI()
     {
         Event currentEvent = Event.current;
 
         if (currentEvent.type == EventType.KeyUp)
-            _isKeyPressed = false;
-        
+        {
+            ReleasePressedKey(currentEvent.keyCode);
+            return;
+        }
+
         if (currentEvent.type != EventType.KeyDown)
             return;
 
-        if (_isKeyPressed)
+        // Ignoring key releasing events.
+        if (IsKeyPressed(currentEvent.keyCode))
             return;
 
-        switch (currentEvent.keyCode)
+        ProcessPressEvent(currentEvent);
+    }
+
+    private void ProcessPressEvent(Event pressEvent)
+    {
+        switch (pressEvent.keyCode)
         {
             case KeyCode.Return:
             {
+                AddPressedKey(pressEvent.keyCode);
                 ReturnPressed?.Invoke(this, EventArgs.Empty);
-                _isKeyPressed = true;
                 break;
             }
             case KeyCode.Backspace:
             {
+                AddPressedKey(pressEvent.keyCode);
                 BackspacePressed?.Invoke(this, EventArgs.Empty);
-                _isKeyPressed = true;
                 break;
             }
             default:
             {
-                if (currentEvent.character != 0)
-                {
-                    KeyPressed?.Invoke(this, currentEvent.character);
-                    _isKeyPressed = true;
-                }
+                if (pressEvent.character is NullCharacter or ReturnCharacter)
+                    break;
+
+                AddPressedKey(pressEvent.keyCode);
+                KeyPressed?.Invoke(this, pressEvent.character);
                 break;
             }
         }
+    }
+    
+    private void AddPressedKey(KeyCode keyCode)
+    {
+        if (keyCode == KeyCode.None)
+            return;
+        
+        _pressedKeyCodes.Add(keyCode);
+    }
+
+    private bool IsKeyPressed(KeyCode keyCode)
+    {
+        return _pressedKeyCodes.Contains(keyCode);
+    }
+
+    private void ReleasePressedKey(KeyCode keyCode)
+    {
+        _pressedKeyCodes.Remove(keyCode);
     }
 }
