79f3def17cb88e15278908490e61f98cba5ea921
XDenizX <denizik1@mail.ru>
2023-02-09 22:00:52 +0300

    Updated game constant initialization.

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -4,6 +4,6 @@ public static class GameConstants
 {
     public const int SmallScore = 100;
     public const int BigScore = 300;
-    public static TimeSpan SmallTime = new(0, 0, 5);
-    public static TimeSpan BigTime = new(0, 0, 10);
+    public static TimeSpan SmallTime = TimeSpan.FromSeconds(5);
+    public static TimeSpan BigTime = TimeSpan.FromSeconds(10);
 }
